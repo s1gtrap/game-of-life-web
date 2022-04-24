88f8d8454a050d8a956c2635a61b782e1a08ff4c
William Tange <will@brt.sexy>
2022-04-24 15:46:39 +0200

    Pass dims and initial states by message

--- www/index.js
+++ www/index.js
@@ -8,20 +8,33 @@ async function run_wasm() {
     // `wasm_bindgen` was imported in `index.html`
     //await wasm_bindgen('./pkg/game_of_life_web_bg.wasm');
 
-    const width = 1000;
-    const height = 1000;
+    const width = 5;
+    const height = 5;
 
     const w = new Worker('./worker.js');
-    console.log('index.js loaded');
     let c = document.createElement('canvas');
     c.width = width;
     c.height = height;
+    c.style.width = '100%';
+    c.style.height = '100%';
+    c.style['image-rendering'] = 'pixelated';
     let ctx = c.getContext('2d');
     let id = new ImageData(width, height);
-    w.onmessage = (e) => {
-        id.data.set(e.data[1]);
+    //id.data.set([120, 180, 44, 255, 99, 110, 255, 255, 0, 0, 255, 255, 255, 0, 0, 255])
+    //ctx.putImageData(id, 0, 0);
+    w.addEventListener('message', (e) => {
+        id.data.set(e.data);
         ctx.putImageData(id, 0, 0);
-    };
+    });
+    const states = [
+        0, 0, 0, 0, 0,
+        0, 0, 1, 0, 0,
+        0, 0, 1, 0, 0,
+        0, 0, 1, 0, 0,
+        0, 0, 0, 0, 0,
+    ].map((d) => [0, 0, 0, d * 255]).flat();
+    console.log(states.length);
+    w.postMessage({ width, height, states });
     document.body.addEventListener('click', () => {
         w.postMessage(null);
     })
